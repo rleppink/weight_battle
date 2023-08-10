dd14f607c2065d0b95617e031204313ea643d16f
Ronald Leppink <rleppink@Ronalds-MacBook-Air.local>
2023-08-10 15:47:40 +0200

    Create simple MVP

--- lib/weight_battle_web/controllers/page_controller.ex
+++ lib/weight_battle_web/controllers/page_controller.ex
@@ -1,9 +1,52 @@
 defmodule WeightBattleWeb.PageController do
   use WeightBattleWeb, :controller
 
+  @data %{
+    "Laura" => %{
+      goal: 55.0,
+      data_points: [
+        %{
+          date: NaiveDateTime.new!(~D[2023-08-07], ~T[08:00:00]),
+          weight: 59.9
+        },
+        %{
+          date: NaiveDateTime.new!(~D[2023-08-08], ~T[08:00:00]),
+          weight: 59.4
+        },
+        %{
+          date: NaiveDateTime.new!(~D[2023-08-09], ~T[08:00:00]),
+          weight: 59.0
+        },
+        %{
+          date: NaiveDateTime.new!(~D[2023-08-10], ~T[08:00:00]),
+          weight: 58.7
+        }
+      ]
+    },
+    "Ronald" => %{
+      goal: 100.0,
+      data_points: [
+        %{
+          date: NaiveDateTime.new!(~D[2023-08-07], ~T[08:00:00]),
+          weight: 108.5
+        },
+        %{
+          date: NaiveDateTime.new!(~D[2023-08-08], ~T[08:00:00]),
+          weight: 108.2
+        },
+        %{
+          date: NaiveDateTime.new!(~D[2023-08-09], ~T[08:00:00]),
+          weight: 107.6
+        },
+        %{
+          date: NaiveDateTime.new!(~D[2023-08-10], ~T[08:00:00]),
+          weight: 108.1
+        }
+      ]
+    }
+  }
+
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn |> assign(:data, @data), :home)
   end
 end
